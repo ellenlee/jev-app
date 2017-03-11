cd261ac89e751ed561a730a0da3cdc0b6f520668
Ellen Lee <leechinghui.tw@gmail.com>
2017-03-12 02:23:39 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,8 @@ Rails.application.routes.draw do
     root 'users#index'
 
     resources :users
-<<<<<<< HEAD
     resources :tracks do
       resources :sections
-=======
-<<<<<<< HEAD
-    resources :tracks do
-      resources :sections
-=======
-    resources :tracks
-    resources :sections do
->>>>>>> bb05fd8a99376555315a99c0966d110769859c58
->>>>>>> 79aa8ecb1e6c4f58fd4c313402ced5c637f6eef7
       resources :assignments
       resources :quizzes
     end
