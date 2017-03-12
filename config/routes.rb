83017ac5142e4e2a5f815632016de91539e7fcf3
Ellen Lee <leechinghui.tw@gmail.com>
2017-03-12 23:59:22 +0800

    write page guide

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :tracks
 
-  root to: 'tracks#index'
+
 
   namespace :admin do
     root 'users#index'
@@ -15,4 +15,8 @@ Rails.application.routes.draw do
       resources :quizzes
     end
   end
+
+  get '/guide/welcome' => 'guide#welcome'
+
+  root to: 'guide#index'
 end
